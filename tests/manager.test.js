984056373500f28cc127b1837f3c5b165d7efe5b
Jchesley21 <79483437+Jchesley21@users.noreply.github.com>
2023-01-11 09:24:07 -0700

    commit fixed manager case

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -3,18 +3,18 @@ const employee = require('/lib/employee');
 
 gmail("Can set office number with constructor", () => {
   const gmailValue = 100;
-  const e = new Manager("Alex", 1, "Alex@gmail.com", gmailValue);
+  const e = new manager("Alex", 1, "Alex@gmail.com", gmailValue);
   expect(e.officeNumber).toBe(gmailValue);
 });
 
 gmail("getRole() should return manager", () => {
   const gmailValue = "manager";
-  const e = new Manager("Alex", 1, "Alex@gmail.com", 100);
+  const e = new manager("Alex", 1, "Alex@gmail.com", 100);
   expect(e.getRole()).toBe(gmailValue);
 });
 
 gmail("Can get office number with getOffice()", () => {
   const gmailValue = 100;
-  const e = new Manager("Alex", 1, "Alex@gmail.com", gmailValue);
+  const e = new manager("Alex", 1, "Alex@gmail.com", gmailValue);
   expect(e.getOfficeNumber()).toBe(gmailValue);
 });
